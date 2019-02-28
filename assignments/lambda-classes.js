3ad91b131466f1faae6e4318e25be6a4369eaf65
Sean Doyle <sdoylelambda@gmail.com>
2019-02-28 18:17:51 -0500

    Student class completed.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -13,18 +13,38 @@ class Person {
 }
 
 class Instructor extends Person {
-    constructor(personAttributes) {
-        super(personAttributes)
-        this.specialty = personAttributes.specialty;
-        this.favLanguage = personAttributes.favLanguage;
-        this.catchPhrase = personAttributes.catchPhrase;
+    constructor(instructorAttributes) {
+        super(instructorAttributes)
+        this.specialty = instructorAttributes.specialty;
+        this.favLanguage = instructorAttributes.favLanguage;
+        this.catchPhrase = instructorAttributes.catchPhrase;
     }
 
     demo(subject) {
         return 'Today we are learning about {subject}.'; 
     }
     grade(student, subject) {
-        return '{student.name} receives a perfect score on {subject}';
+        return '{student.name} receives a perfect score on {subject}.';
+    }
+}
+
+class Student extends Person {
+    constructor(studentAttributes) {
+        super(studentAttributes);
+        this.previousBackground = studentAttributes.previousBackground;
+        this.className = studentAttributes.className;
+        this.favSubject = studentAttributes.favSubject;
+    }
+
+    listsSubjects() {
+        let i = this.favSubject.join(", ")
+        console.log(x)
+    }
+    PRAssignment(subject) {
+        console.log(`${student.name} has submitted a PR for ${subject}.`);
+    }
+    sprintChallenge(subject) {
+        console.log(`${student.name} has begun sprint challenge on ${subject}.`);
     }
 }
 
