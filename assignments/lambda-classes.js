643661301af19ad386f4ec1780a1c3c9b4f0c89a
Sean Doyle <sdoylelambda@gmail.com>
2019-02-28 17:56:44 -0500

    Person and Instructor classes completed.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,30 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+    this.gender = attributes.gender;
+    }
+    speak() {
+        return `Hello m name is ${this.name}, I am from ${location}.`;
+    }
+}
+
+class Instructor extends Person {
+    constructor(personAttributes) {
+        super(personAttributes)
+        this.specialty = personAttributes.specialty;
+        this.favLanguage = personAttributes.favLanguage;
+        this.catchPhrase = personAttributes.catchPhrase;
+    }
+
+    demo(subject) {
+        return 'Today we are learning about {subject}.'; 
+    }
+    grade(student, subject) {
+        return '{student.name} receives a perfect score on {subject}';
+    }
+}
+
