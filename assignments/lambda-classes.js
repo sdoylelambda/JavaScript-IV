d44fcd99fb57c78256b2319410f6ae3019eb0c5e
Sean Doyle <sdoylelambda@gmail.com>
2019-02-28 19:07:36 -0500

    Class completed.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -8,7 +8,7 @@ class Person {
     this.gender = attributes.gender;
     }
     speak() {
-        return `Hello m name is ${this.name}, I am from ${location}.`;
+        console.log(`Hello m name is ${this.name}, I am from ${location}.`);
     }
 }
 
@@ -21,10 +21,12 @@ class Instructor extends Person {
     }
 
     demo(subject) {
-        return 'Today we are learning about {subject}.'; 
+        console.log(`Today we are learning about ${subject}.`); 
     }
-    grade(student, subject) {
-        return '{student.name} receives a perfect score on {subject}.';
+    //grade(student, subject) {
+    grade(obj) {
+        const {student, subject} = obj
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
     }
 }
 
@@ -37,14 +39,66 @@ class Student extends Person {
     }
 
     listsSubjects() {
-        let i = this.favSubject.join(", ")
-        console.log(x)
+        let i = this.favSubject.join("\n")
+        console.log(i)
     }
-    PRAssignment(subject) {
+    PRAssignment(student, subject) {
         console.log(`${student.name} has submitted a PR for ${subject}.`);
     }
-    sprintChallenge(subject) {
+    sprintChallenge(student, subject) {
         console.log(`${student.name} has begun sprint challenge on ${subject}.`);
     }
 }
 
+class ProjectManager extends Instructor {
+    constructor(PMAttributes) {
+        super(PMAttributes);
+        this.gradClass = PMAttributes.gradClass;
+        this.favInstructor = PMAttributes.favInstructor;
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`);
+    }
+    debugsCode(student) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+const josh = new Instructor({
+    name: 'Josh',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  const liz = new ProjectManager({
+    name: 'Liz',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    favInstructor: `Josh`
+  });
+
+
+const sean = new Student({
+    name: 'Sean',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    specialty: 'JavaScript',
+    previousBackground: 'sales',
+    className: `yo yo`,
+    favSubject: 'CSS',
+  });
+
+
+  console.log(sean.favSubject);
+  console.log(liz.favInstructor);
+  console.log(josh.specialty);
+  console.log(sean.location);
